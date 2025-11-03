9ddd8a6cccad6fcf9c1e84302babbe2ebafc1f92
Bhanutejareddyk <bhanu93534@gmail.com>
2025-11-03 17:50:52 +0530

    Add files via upload

---
+++ fever.c
@@ -0,0 +1,34 @@
+//Program Description
+//Pavan is not feeling well today.He measured his body temperature using a thermometer and it came out to be X °F.
+//A person is said to have fever if his body temperature is strictly greater than 98 °F.
+//Input Format
+//A single line of input contains an integer x.
+//Output Format
+//Print the output as YES or NO.
+//Constraints
+//0<=X<=500
+//Input-1
+//102
+//Output-1
+//YES
+//Input-2
+//100
+//Output-2
+//YES
+//Input-3
+//98
+//Output-3
+//NO
+//---------------------------------------------------------------------------------------------------------------
+#include<stdio.h>
+int main(){
+int f;
+scanf("%d",&f);
+if(f>98){
+printf("YES");
+}
+else{
+printf("NO");
+}
+return 0;
+}
